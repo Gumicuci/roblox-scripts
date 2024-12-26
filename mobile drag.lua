49ff1b4c18b6a30f30f17d6425f2e615e77511a1
Gumicuci <gumicuci2@gmail.com>
2024-12-26 15:17:01 +0000

    Update mobile drag.lua

--- mobile drag.lua
+++ mobile drag.lua
@@ -7,13 +7,13 @@ local UICorner = Instance.new("UICorner")
 freaky.Name = "freaky"
 freaky.Parent = game.CoreGui
 freaky.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-freaky.DisplayOrder = 750
+freaky.DisplayOrder = 999
 
 ImageButton.Parent = freaky
 ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton.BorderSizePixel = 0
-ImageButton.Position = UDim2.new(0.125386998, 0, 0.493718594, 0)
+ImageButton.Position = UDim2.new(0.942825675, 0, 0.588426113, 0)
 ImageButton.Size = UDim2.new(0, 31, 0, 31)
 ImageButton.Image = "rbxassetid://73661126234408"
 
@@ -21,12 +21,12 @@ UICorner.Parent = ImageButton
 
 -- Scripts:
 
-local function AUWJTJC_fake_script() -- ImageButton.LocalScript 
+local function FYLBSV_fake_script() -- ImageButton.LocalScript 
 	local script = Instance.new('LocalScript', ImageButton)
 
 	local guiObject = script.Parent
-
-	guiObject.TouchTap:Connect(function()
+	
+	guiObject.MouseButton1Click:Connect(function()
 		for _, v in pairs(game:GetService("CoreGui").ScreenGui:GetChildren()) do
 			if v:FindFirstChild("TextLabel") then
 				v.Visible = not v.Visible
@@ -34,4 +34,4 @@ local function AUWJTJC_fake_script() -- ImageButton.LocalScript
 		end
 	end)
 end
-coroutine.wrap(AUWJTJC_fake_script)()
+coroutine.wrap(FYLBSV_fake_script)()
